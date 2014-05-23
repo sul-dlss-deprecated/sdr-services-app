102a15955b3a91093646d3fc7025b361ec7f2e8c
Richard Anderson <rnanders@stanford.edu>
2014-05-23 16:09:57 -0600

    Version bump, edited Capistrano deploy_to directive

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'https://github.com/sul-dlss/sdr-services-app.git'
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, "/var/#{fetch(:user)}/#{fetch(:application)}"
+set :deploy_to, "/var/sdr2service/#{fetch(:application)}"
 
 # Default value for :scm is :git
 # set :scm, :git
