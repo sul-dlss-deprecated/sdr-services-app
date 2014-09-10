edcfa15d0da91163fece3e3d6041d46bf3221f93
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2014-09-10 15:47:11 -0700

    Minor change to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,16 +2,19 @@
 lock '3.2.1'
 
 set :application, 'sdr-services-app'
-set :repo_url, 'https://github.com/sul-dlss/sdr-services-app.git'
-
-# Default branch is :master
-ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, "/var/sdr2service/#{fetch(:application)}"
 
 # Default value for :scm is :git
 # set :scm, :git
+
+# NOTE: production is not working with https (old openssl?)
+# set :repo_url, 'https://github.com/sul-dlss/sdr-services-app.git'
+set :repo_url, 'git://github.com/sul-dlss/sdr-services-app.git'
+
+# Default branch is :master
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default value for :format is :pretty
 # set :format, :pretty
