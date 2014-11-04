8d0bb222054e6c3f2fcc07776c61456dd8be7354
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2014-11-03 20:43:46 -0800

    ArchiveCatalog - configure Sequel for mysql on local env, use default table names

--- lib/sdr/archive_catalog.rb
+++ lib/sdr/archive_catalog.rb
@@ -25,9 +25,16 @@ module Sdr
 
     # run rspec on RACK_ENV=local, set in config/boot.rb
     configure :local do
+      require 'pry'
       register Sinatra::Reloader
       # connect to an in-memory database
-      DB = Sequel.sqlite
+      #DB = Sequel.sqlite
+      db_config = db_config['local']
+      DB = Sequel.mysql(:host=>db_config['host'],
+                         :port=>db_config['port'],
+                         :user=>db_config['user'],
+                         :password=>db_config['password'],
+                         :database=>db_config['database'])
       # TODO: define archive catalog tables for sqlite
       # # create an items table
       # DB.create_table :items do
@@ -80,49 +87,63 @@ module Sdr
     # Ensure the connection is good on startup, raises exceptions on failure
     puts "#{DB} connected: #{DB.test_connection}"
 
-
     # digital_objects 	All SDR and DPN objects preserved in SDR Core
-    # digital_object_id 	varchar2(40 byte) 	no 	Either the SDR druid or the DPN UUID
-    # home_repository 	varchar2(3 byte) 	no 	The source location of the object/version (sdr or dpn)
+    # digital_object_id 	Either the SDR druid or the DPN UUID
+    # home_repository 	The source location of the object/version (sdr or dpn)
     class DigitalObject < Sequel::Model
-      #set_primary_key [:digital_object_id]
-      #set_primary_key [:category, :title]
     end
-    dObj = DigitalObject.all.first
-    puts "#{dObj.pk}"
+    m = DigitalObject
+    puts "table: #{m.table_name}, columns: #{m.columns}, pk: #{m.primary_key}"
 
     # sdr_objects 	Inventory of SDR objects preserved in SDR Core
     class SdrObject < Sequel::Model
     end
+    m = SdrObject
+    puts "table: #{m.table_name}, columns: #{m.columns}, pk: #{m.primary_key}"
 
     # sdr_object_versions 	Inventory of all versions of SDR objects
-    class SdrObjectVersion < Sequel::Model(sdr_object_versions)
+    class SdrObjectVersion < Sequel::Model
     end
+    m = SdrObjectVersion
+    puts "table: #{m.table_name}, columns: #{m.columns}, pk: #{m.primary_key}"
 
     # sdr_version_stats 	Size metrics for SDR object versions
-    class SdrVersionStat < Sequel::Model(:sdr_version_stats)
+    class SdrVersionStat < Sequel::Model
     end
+    m = SdrVersionStat
+    puts "table: #{m.table_name}, columns: #{m.columns}, pk: #{m.primary_key}"
 
     # replicas 	Containerized copies of object/versions (to be) archived to tape
-    class Replica < Sequel::Model(:replicas)
+    class Replica < Sequel::Model
     end
+    m = Replica
+    puts "table: #{m.table_name}, columns: #{m.columns}, pk: #{m.primary_key}"
 
     # tape_replicas 	Join table indicating which replicas were archived in which tape archive
-    class TapeReplica < Sequel::Model(:tape_replicas)
+    class TapeReplica < Sequel::Model
     end
+    m = TapeReplica
+    puts "table: #{m.table_name}, columns: #{m.columns}, pk: #{m.primary_key}"
+
     # tape_archives 	A container holding multiple replicas which was archived to tape
-    class TapeArchive < Sequel::Model(:tape_archives)
+    class TapeArchive < Sequel::Model
     end
+    m = TapeArchive
+    puts "table: #{m.table_name}, columns: #{m.columns}, pk: #{m.primary_key}"
 
     # dpn_objects 	Inventory of objects from DPN that were archived to tape
-    class DPNObject < Sequel::Model(:dpn_objects)
+    class DpnObject < Sequel::Model
     end
+    m = DpnObject
+    puts "table: #{m.table_name}, columns: #{m.columns}, pk: #{m.primary_key}"
 
     # Views:
     #
-    # new_replicas 	Replicas not yet archived to tape 	details
-    class NewReplica < Sequel::Model(:new_replicas)
+    # new_replicas 	Replicas not yet archived to tape
+    class NewReplica < Sequel::Model
     end
+    m = NewReplica
+    puts "table: #{m.table_name}, columns: #{m.columns}, pk: #{m.primary_key}"
 
   end
 
