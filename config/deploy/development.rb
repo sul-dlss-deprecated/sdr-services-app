dc663ab6f0fc4b48af7d1a5476ecd39bdb852f17
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-01-09 10:40:11 -0800

    Refactor deployment configs

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,20 +1,10 @@
 # Note: capistrano reads this file AFTER config/deploy.rb
 
 ENV['SDR_APP']  ||= fetch(:application)
-ENV['SDR_HOST'] ||= 'hostname'
-ENV['SDR_USER'] ||= 'username'
+ENV['SDR_HOST'] ||= 'localhost'
+ENV['SDR_USER'] ||= ENV['USER']
+ENV['SDR_URL']  ||= "http://#{ENV['SDR_HOST']}/sdr"
 
 puts File.expand_path(__FILE__)
-puts "ENV['SDR_APP']  = #{ENV['SDR_APP']}"
-puts "ENV['SDR_HOST'] = #{ENV['SDR_HOST']}"
-puts "ENV['SDR_USER'] = #{ENV['SDR_USER']}"
-puts
-
-set :default_env, {
-    'SDR_APP'  => ENV['SDR_APP'],
-    'SDR_USER' => ENV['SDR_USER'],
-    'SDR_HOST' => ENV['SDR_HOST'],
-}
-
 require_relative 'server_settings'
 
--- config/deploy/server_settings.rb
+++ config/deploy/server_settings.rb
@@ -1,5 +1,18 @@
 # Note: this file is required in config/deploy/<environment>.rb
 
+puts "ENV['SDR_APP']  = #{ENV['SDR_APP']}"
+puts "ENV['SDR_USER'] = #{ENV['SDR_USER']}"
+puts "ENV['SDR_HOST'] = #{ENV['SDR_HOST']}"
+puts "ENV['SDR_URL']  = #{ENV['SDR_URL']}"
+puts
+
+set :default_env, {
+  'SDR_APP'  => ENV['SDR_APP'],
+  'SDR_USER' => ENV['SDR_USER'],
+  'SDR_HOST' => ENV['SDR_HOST'],
+  'SDR_URL'  => ENV['SDR_URL'],
+}
+
 server ENV['SDR_HOST'], user: ENV['SDR_USER'], roles: %w{app}
 Capistrano::OneTimeKey.generate_one_time_key!
 ssh_opts = fetch(:ssh_options)
