a33449fac3ffbd3ab1bb4c9a40bc973ff1a336cb
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-02-05 13:41:21 -0800

    Allow 'test' environment into repo

---
+++ config/environments/test.rb
@@ -0,0 +1,16 @@
+
+Moab::Config.configure do
+  storage_roots File.join(File.dirname(__FILE__), '..', '..', 'spec', 'fixtures')
+  storage_trunk 'repository'
+  deposit_trunk nil
+  path_method :druid
+end
+
+SdrServices::Config.configure do
+  username 'devUser'
+  password 'devPass'
+  admin_email ENV['USER']
+  storage_filesystems ['/tmp']
+  rsync_destination_host 'localhost'
+  rsync_destination_path '/tmp/sdr_transfer'
+end
