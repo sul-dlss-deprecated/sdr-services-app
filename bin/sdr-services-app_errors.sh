c273050622bf972adc67b6400f8064396ec06a23
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-02-05 13:24:22 -0800

    Log utity: cleanup vars

--- bin/sdr-services-app_errors.sh
+++ bin/sdr-services-app_errors.sh
@@ -13,15 +13,10 @@ LOG_FILES="${LOG_PATH}/sdr.log"
 
 today=$(date +%d/%b/%Y)
 
-regex_ip='s/([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*/\1/'
-
-#regex_druid='[[:lower:]]{2}[[:digit:]]{3}[[:lower:]]{2}[[:digit:]]{4}'
-regex_druid='[a-z]{2}[0-9]{3}[a-z]{2}[0-9]{4}'
-
-for f in $LOG_FILES; do
+for f in ${LOG_FILES}; do
 	echo -e "\n\n********************************************************************************"
 	echo -e "ERRORS on $today for $f\n"
-	grep "$today" $f | grep -v 'ObjectNotFoundException' | grep -B1 -A2 'ERROR.*message'
+	grep "$today" ${f} | grep -v 'ObjectNotFoundException' | grep -B1 -A2 'ERROR.*message'
 done
 echo
 echo
