b8c47fb4db54e7e449c25d89b6839e0e05ad9e9b
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-02-05 21:15:59 -0800

    Modify SQL scripts to init mysql db with idempotent operations

--- db/mysql_db_create.sql
+++ db/mysql_db_create.sql
@@ -9,8 +9,10 @@ CREATE DATABASE archive_catalog_development
     DEFAULT CHARACTER SET utf8
     DEFAULT COLLATE utf8_general_ci;
 
-DROP USER 'sdrAdmin'@'localhost';
-CREATE USER 'sdrAdmin'@'localhost' IDENTIFIED BY 'sdrPass';
-GRANT ALL PRIVILEGES ON archive_catalog_test.* TO 'sdrAdmin'@'localhost';
-GRANT ALL PRIVILEGES ON archive_catalog_development.* TO 'sdrAdmin'@'localhost';
+# DROP USER 'sdrAdmin'@'localhost';
+# CREATE USER 'sdrAdmin'@'localhost' IDENTIFIED BY 'sdrPass';
+GRANT ALL PRIVILEGES ON archive_catalog_test.*
+  TO 'sdrAdmin'@'localhost' IDENTIFIED BY 'sdrPass';
+GRANT ALL PRIVILEGES ON archive_catalog_development.*
+  TO 'sdrAdmin'@'localhost' IDENTIFIED BY 'sdrPass';
 
