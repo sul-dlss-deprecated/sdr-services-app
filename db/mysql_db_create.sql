d6c52afd196641f8ea605bba0e58ac04b2dccbaf
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2015-02-05 20:46:37 -0800

    Add db/mysql* init SQL scripts

---
+++ db/mysql_db_create.sql
@@ -0,0 +1,16 @@
+
+DROP DATABASE archive_catalog_test;
+CREATE DATABASE archive_catalog_test
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
+
+DROP DATABASE archive_catalog_development;
+CREATE DATABASE archive_catalog_development
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
+
+DROP USER 'sdrAdmin'@'localhost';
+CREATE USER 'sdrAdmin'@'localhost' IDENTIFIED BY 'sdrPass';
+GRANT ALL PRIVILEGES ON archive_catalog_test.* TO 'sdrAdmin'@'localhost';
+GRANT ALL PRIVILEGES ON archive_catalog_development.* TO 'sdrAdmin'@'localhost';
+
