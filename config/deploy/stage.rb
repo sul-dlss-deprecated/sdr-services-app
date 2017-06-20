cedec804c7f9d25af8ecf7c78f47ddf2a56b39e8
Justin Coyne <jcoyne@justincoyne.com>
2017-06-20 13:56:52 -0500

    Update capistrano deployment
    See https://github.com/sul-dlss/sdr-services-app/pull/17

---
+++ config/deploy/stage.rb
@@ -0,0 +1,7 @@
+ENV['APP_ENV'] ||= 'staging'
+
+set :default_env, 'APP_ENV' => ENV['APP_ENV']
+
+server 'sdr-services-test.stanford.edu', user: 'sdr2service', roles: %w{app}
+
+Capistrano::OneTimeKey.generate_one_time_key!
